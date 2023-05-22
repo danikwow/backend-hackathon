22b50ce125c95f6b3c63ed4af82823a046433a65
danikwow <122081538+danikwow@users.noreply.github.com>
2023-05-22 20:35:41 +0700

    Добавил CRUD методы.

--- src/main/java/com/digital_guru/hackathonbackend/repo/NewsRepository.java
+++ src/main/java/com/digital_guru/hackathonbackend/repo/NewsRepository.java
@@ -1,4 +1,7 @@
 package com.digital_guru.hackathonbackend.repo;
 
-public interface NewsRepository {
+import com.digital_guru.hackathonbackend.entity.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewsRepository extends JpaRepository<News, Long> {
 }
